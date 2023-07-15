e892e73020647a410feb43cf476342c51aa730ad
Janesarai <saraipalaciosjane@gmail.com>
2023-07-14 21:22:14 -0600

    trece a listo

--- app/src/main/java/com/example/trecekotlin/Trece2.kt
+++ app/src/main/java/com/example/trecekotlin/Trece2.kt
@@ -1,5 +1,7 @@
 package com.example.trecekotlin
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 class Trece2 {
@@ -9,8 +11,19 @@ fun main() {
     println(resultado)
     var redonde = redondeo(param1 = 7.6)
     println(redonde)
+    var muestraResult= muestraResultado(8.57)
+    println(muestraResult)
 }
 
 fun suma(param1: Double, param2: Float): Double = param1 + param2
 
 fun redondeo(param1: Double) = ceil(param1)
+
+fun muestraResultado(numero: Double) : String {
+
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("EUR"))
+    return format.format(numero)
+
+}
